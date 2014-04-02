8c8b7348ed5aa7ac686b1930da5130deec7dd2bb
Asad Amjad <asad_dbest@yahoo.com>
2014-04-02 23:38:15 +0500

    First

---
+++ calc.rb
@@ -0,0 +1,8 @@
+puts "enter 1st number = "
+x=gets.chomp.to_i
+puts "enter 2nd number = "
+y=gets.chomp.to_i
+puts "1st+2nd = #{x+y}"
+puts "1st*2nd = #{x*y}" 
+puts "1st-2nd = #{x-y}"
+puts "1st/2nd = #{x/y}"
